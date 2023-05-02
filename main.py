a237f8f870bd870d1911b6f1de921d84ce94eb99
Le Quang Tho <92069270+letho1608@users.noreply.github.com>
2023-05-03 03:18:08 +0700

    Update and rename simulation_btc.py to main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import time
 import random
 from termcolor import colored
 
-max_nonce = 2 ** 64
-difficulty = 4
+max_nonce = 2 ** 32
+difficulty = 5.00
 
 version = 1
 previous_block_hash = "00000000000000000006a78e7de1d304b1d2f0c5bca617b0e8c88d2399be5d5"
@@ -17,7 +17,7 @@ total_bitcoin = 0
 while True:
     block_header = f"{version}{previous_block_hash}{merkle_root}{timestamp}{bits}{nonce}".encode('utf-8')
     hash = hashlib.sha256(hashlib.sha256(block_header).digest()).hexdigest()
-    if hash.startswith('0' * difficulty):
+    if hash.startswith('0' * int(difficulty)):
         block_reward = random.uniform(0.00000000001, 0.00000000003)
         total_bitcoin += block_reward
         total_bitcoin_str = "{:.11f}".format(total_bitcoin)
@@ -26,7 +26,7 @@ while True:
         nonce = 0
         time.sleep(1)
         if int(total_bitcoin * 100000000) % 100000 == 0:
-            difficulty += 1
+            difficulty += 0.01
     else:
         nonce += 1
         if nonce >= max_nonce:
